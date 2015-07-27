0e5260d698bb79e4f6122dae6cdce7090b2979d6
Maik Wojcieszak <mw@wobe-team.com>
2015-07-27 09:40:07 +0200

    ported half precision float windows visual studio - added portable is isinf/isnan

--- src/ujo_float_half.h
+++ src/ujo_float_half.h
@@ -1,10 +1,49 @@
+/* 
+ *  LibUjo:  An UJO binaray data object notation library.
+ *  Copyright (c) 2015 by wobe-systems GmbH
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
+ *  
+ *  You may find a copy of the license under this software is released
+ *  at COPYING file. This is LGPL software: you are welcome to develop
+ *  proprietary applications using this library without any royalty or
+ *  fee but returning back any change, improvement or addition in the
+ *  form of source code, project image, documentation patches, etc.
+ *
+ *  Homepage:
+ *    http://www.libujo.org
+ *
+ *  For professional support contact us:
+ *
+ *    wobe-systems GmbH
+ *    support@libujo.org
+ *
+ *  Contributers:
+ *		Created on: 02.07.2015
+ *			Author: akristmann
+ */
+
 #ifndef _UJO_FLOAT__HALF_H_
 #define _UJO_FLOAT__HALF_H_
 
-#include <stdint.h>
 #include <stddef.h>
 
 #include "ujo_types.h"
+
+typedef uint16_t float16_t;
 
 BEGIN_C_DECLS
 
@@ -12,13 +51,10 @@ float16_t float_to_half(float f);
 
 float half_to_float(float16_t h);
 
-//unsigned int convertmantissa(unsigned int i);
+int isinf(float32_t x);
 
-//void printBits(const size_t  size, void const * const ptr);
+int isnan(float32_t x);
 
-//void generatetables_float_to_half();
-
-//void generatetables_half_to_float();
 
 END_C_DECLS
 
