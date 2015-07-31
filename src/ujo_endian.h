ebb481d8b96030a226237e87857dcf88dcdb034c
Arthur Kristmann <ak@tml-software.com>
2015-07-31 11:45:32 +0200

    Big-endian support #3
    Initial commit for big-endian support

---
+++ src/ujo_endian.h
@@ -0,0 +1,45 @@
+/*
+ *  LibUjo:  An UJO binaray data object notation library.
+ *  Copyright (c) 2015 by wobe-systems GmbH
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
+ *
+ *  You may find a copy of the license under this software is released
+ *  at COPYING file. This is LGPL software: you are welcome to develop
+ *  proprietary applications using this library without any royalty or
+ *  fee but returning back any change, improvement or addition in the
+ *  form of source code, project image, documentation patches, etc.
+ *
+ *  Homepage:
+ *    http://www.libujo.org
+ *
+ *  For professional support contact us:
+ *
+ *    wobe-systems GmbH
+ *    support@libujo.org
+ *
+ *  Contributers:
+ *      Created on: 31.07.2015
+ *          Author: akristmann
+ */
+
+#ifndef _UJO_ENDIAN_H_
+#define _UJO_ENDIAN_H_
+
+
+
+
+#endif // _UJO_ENDIAN_H_
